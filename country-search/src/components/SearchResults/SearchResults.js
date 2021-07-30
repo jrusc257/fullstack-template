c8440fe5cca6f6467ec73221c2af0fca3a5adbf4
John Rusch <john.rusch.mke@gmail.com>
2021-07-29 21:40:35 -0500

    Cleanup

--- country-search/src/components/SearchResults/SearchResults.js
+++ country-search/src/components/SearchResults/SearchResults.js
@@ -4,13 +4,16 @@ import LoadingIndicator from '../LoadingIndicator/LoadingIndicator';
 import './SearchResults.scss';
 
 const SearchResults = ({listResults, loading}) => {
-
     if (loading === false) {
         if (listResults !== undefined && listResults !== null) {
             console.log("Result Details", listResults);
             return (
                 listResults.map((country, key) => {
-                    return <CountryListing key={country.name} countryDetails={country} />
+                    return (
+                        <div>
+                            <CountryListing key={country.name} countryDetails={country} />
+                        </div>
+                    )
                 })
             )
         } else {
